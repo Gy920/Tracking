47638154ec92a3df02475f30b1536b08ee4494e1
Verse <2712089295@qq.com>
2023-01-13 15:45:52 +0800

    add demo

--- demo/TrackingDemo.cpp
+++ demo/TrackingDemo.cpp
@@ -2,6 +2,21 @@
 #include <opencv4/opencv2/opencv.hpp>
 #include <chrono>
 #include <iostream>
+
+void getDetectBox(int idex, std::vector<cv::Rect2d> &detect_box)
+{
+    detect_box.clear();
+    if (idex >= 15 && idex <= 53 && idex % 2 == 1) //每两帧检测到一次
+    {
+        detect_box.push_back(cv::Rect2d(0 + 25 * (idex - 15), 250, 320, 230));
+    }
+    if (idex >= 42 && idex <= 72 && idex % 3 != 1) //每三帧检测到一次
+    {
+        detect_box.push_back(cv::Rect2d(0 + 32 * (idex - 42), 80, 290, 200));
+    }
+}
+
+// 红色为detect box ,蓝色为 correct box
 int main(int argc, char *argv[])
 {
 
@@ -21,14 +36,38 @@ int main(int argc, char *argv[])
     }
     cv::Mat frame;
     cap >> frame;
-    cv::namedWindow("tracking demo", cv::WINDOW_NORMAL);
+    cv::namedWindow("tracking_demo", cv::WINDOW_NORMAL);
+    std::cout << frame.cols << " " << frame.rows << std::endl;
+    int idex = 0;
+    std::vector<cv::Rect2d> detect_box;
+    std::vector<Track::TrackingBox> update_box;
 
     while (1)
     {
+        idex++;
         cap >> frame;
+        if (frame.empty() || idex == 73)
+        {
+            break;
+        }
+        std::cout << idex << std::endl;
+        //
+        getDetectBox(idex, detect_box);
+        //
+        trakcer->track(frame, detect_box);
+        trakcer->getTrakingResult(update_box);
+
+        for (auto a : detect_box)
+        {
+            cv::rectangle(frame, a, cv::Scalar(255, 0, 255), 5);
+        }
+        for (auto a : update_box)
+        {
+            cv::rectangle(frame, a.box, cv::Scalar(255, 0, 0), 2);
+            cv::putText(frame,std::to_string(a.id),a.box.tl(),2,2,cv::Scalar(255, 0, 0), 2);
+        }
         cv::imshow("tracking_demo", frame);
-        cv::waitKey(1);
-        if (cv::waitKey(1) == 27)
+        if (cv::waitKey(100) == 27)
         {
             break;
         }
